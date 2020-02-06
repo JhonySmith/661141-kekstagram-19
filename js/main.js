368a969522658770bc30534a8ea98fae95b2c082
jhonysmith <evesnin68@gmail.com>
2020-02-06 20:59:47 +0300

    npm tetst

--- js/main.js
+++ js/main.js
@@ -1,23 +1,24 @@
-"use strict";
+'use strict';
 
 var messages = [
-  "Всё отлично!",
-  "В целом всё неплохо.Но не всё.",
-  "Когда вы делаете фотографию, хорошо бы убирать палец из кадра.В конце концов это просто непрофессионально.",
-  "Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.",
-  "Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.",
-  "Лица у людей на фотке перекошены, как будто их избивают.Как можно было поймать такой неудачный момент ? !"
+  'Всё отлично!',
+  'В целом всё неплохо.Но не всё.',
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра.В конце концов это просто непрофессионально.',
+  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+  'Лица у людей на фотке перекошены, как будто их избивают.Как можно было поймать такой неудачный момент ? !'
 ];
 
-var names = ["Петя", "Саша", "Аня", "Коля", "Даша", "Вася", "Настя", "Катя"];
+var names = ['Петя', 'Саша', 'Аня', 'Коля', 'Даша', 'Вася', 'Настя', 'Катя'];
 
-//Функция для создания случайных, неповторяющихся значений
+//  Функция для создания случайных, неповторяющихся значений
+
 function shuffle(array) {
-  var currentIndex = array.length,
-    temporaryValue,
-    randomIndex;
+  var currentIndex = array.length;
+  var temporaryValue;
+  var randomIndex;
 
-  while (0 !== currentIndex) {
+  while (currentIndex !== 0) {
     randomIndex = Math.floor(Math.random() * currentIndex);
     currentIndex -= 1;
 
@@ -40,25 +41,25 @@ var photoGenerator = function (photoArr) {
     shuffle(photoRandom);
   }
 
-  for (var i = 1; i < 7; i++) {
-    avatarRandom.push(i);
+  for (var j = 1; j < 7; j++) {
+    avatarRandom.push(j);
     shuffle(avatarRandom);
   }
 
-  for (var i = 0; i < 25; i++) {
+  for (var k = 0; k < 25; k++) {
     var commentArr = [];
     shuffle(names);
-    for (var j = 0; j < 4; j++) {
-      commentArr[j] = {
-        avatar: "img/avatar-" + avatarRandom[j] + ".svg",
+    for (var c = 0; c < 4; c++) {
+      commentArr[c] = {
+        avatar: 'img/avatar-' + avatarRandom[c] + '.svg',
         message: messages[Math.floor(Math.random() * 6)],
-        name: names[j]
+        name: names[c]
       };
     }
 
-    photoArr[i] = {
-      url: "photos/" + photoRandom[i] + ".jpg",
-      description: "Описание фотографии",
+    photoArr[k] = {
+      url: 'photos/' + photoRandom[k] + '.jpg',
+      description: 'Описание фотографии',
       likes: Math.floor(Math.random() * 186) + 15,
       comments: commentArr
     };
@@ -69,17 +70,17 @@ var photoGenerator = function (photoArr) {
 
 photoGenerator(photos);
 
-var picturesBlock = document.querySelector(".pictures");
+var picturesBlock = document.querySelector('.pictures');
 
-var photoTemplate = document.querySelector("#picture").content.querySelector('.picture');
+var photoTemplate = document.querySelector('#picture').content.querySelector('.picture');
 
 var renderPhotos = function (photo) {
   var photoElement = photoTemplate.cloneNode(true);
 
-  photoElement.querySelector(".picture__img").src = photo.url;
-  photoElement.querySelector(".picture__likes").textContent =
+  photoElement.querySelector('.picture__img').src = photo.url;
+  photoElement.querySelector('.picture__likes').textContent =
     photo.likes;
-  photoElement.querySelector(".picture__comments").textContent =
+  photoElement.querySelector('.picture__comments').textContent =
     photo.comments.length;
 
   return photoElement;
