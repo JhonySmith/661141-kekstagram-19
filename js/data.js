45a90847d69e68a4f108884f5a7dfb3cfd9edce3
jhonysmith <evesnin68@gmail.com>
2020-03-21 14:58:25 +0300

    Убраны мокки

--- js/data.js
+++ js/data.js
@@ -1,23 +1,6 @@
 'use strict';
 
 (function () {
-  var COMMENTS_NUMBER = Math.floor(Math.random() * 4);
-
-  // Входные массивы для формирования готовых данных
-
-  var messages = [
-    'Всё отлично!',
-    'В целом всё неплохо.Но не всё.',
-    'Когда вы делаете фотографию, хорошо бы убирать палец из кадра.В конце концов это просто непрофессионально.',
-    'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
-    'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
-    'Лица у людей на фотке перекошены, как будто их избивают.Как можно было поймать такой неудачный момент ? !'
-  ];
-
-  var names = ['Петя', 'Саша', 'Аня', 'Коля', 'Даша', 'Вася', 'Настя', 'Катя'];
-
-  // Функция перемешивания элементов массива
-
   var shuffle = function (array) {
     var currentIndex = array.length;
     var temporaryValue;
@@ -35,9 +18,7 @@
     return array;
   };
 
-  // Функция получения перемешанного массива численных данных
-
-  var randomUniqArr = function (uniqArr) {
+  var makeRandomUniqArr = function (uniqArr) {
     for (var i = 1; i <= uniqArr.length; i++) {
       uniqArr.push(i);
     }
@@ -45,50 +26,8 @@
     return uniqArr;
   };
 
-  var makePhotoArr = function (photoArr, photosNumber) {
-    var photoRandom = [];
-    var commentArr = [];
-
-    randomUniqArr(photosNumber, photoRandom);
-
-    for (var i = 0; i < photosNumber; i++) {
-      makeCommentsArr(commentArr);
-      var LIKES_NUMBER = Math.floor(Math.random() * 186) + 15;
-
-      photoArr[i] = {
-        url: 'photos/' + photoRandom[i] + '.jpg',
-        description: 'Описание фотографии',
-        likes: LIKES_NUMBER,
-        comments: commentArr
-      };
-    }
-
-    return photoArr;
-  };
-
-  // функция генерации случайных комментариев
-
-  var makeCommentsArr = function (clearCommentsArr) {
-    var avatarRandom = [];
-
-    shuffle(names);
-    randomUniqArr(6, avatarRandom);
-
-    for (var i = 0; i < COMMENTS_NUMBER; i++) {
-      var RANDOM_MESSAGE = Math.floor(Math.random() * 6);
-
-      clearCommentsArr[i] = {
-        avatar: 'img/avatar-' + avatarRandom[i] + '.svg',
-        message: messages[RANDOM_MESSAGE],
-        name: names[i]
-      };
-    }
-    return clearCommentsArr;
-  };
-
   window.data = {
-    makePhotoArr: makePhotoArr,
-    randomUniqArr: randomUniqArr,
+    randomUniqArr: makeRandomUniqArr,
     shuffle: shuffle
   };
 })();
